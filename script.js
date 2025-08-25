59bfd13b78d2dfc8f30189e5ae08d2cf419ccbdd
Isaac Santos <165857633+IsacGss@users.noreply.github.com>
2025-08-25 11:30:15 -0300

    script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ document.addEventListener('DOMContentLoaded', () => {
             title: 'Receitas Saudáveis',
             description: 'Comidas práticas pra sua vibe fit!',
             price: 'R$ 19,90',
-            image: 'images/saude.jpg',
+            image: 'images/Saude.jpg',
             kiwifyLink: 'https://pay.kiwify.com.br/sdee28v',
             trending: true
         },
@@ -93,4 +93,5 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 });
+
 
